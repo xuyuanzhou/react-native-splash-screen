4823bdf4ae1dd2fea878b9c2251efd2d56f15ee0
xuyuanzhou <635043894@qq.com>
2018-04-11 11:22:10 +0800

    增加show方法

--- android/src/main/java/org/devio/rn/splashscreen/SplashScreen.java
+++ android/src/main/java/org/devio/rn/splashscreen/SplashScreen.java
@@ -14,14 +14,13 @@ import java.lang.ref.WeakReference;
  * Email:crazycodeboy@gmail.com
  */
 public class SplashScreen {
-    private static int NULL_ID = 0;
     private static Dialog mSplashDialog;
     private static WeakReference<Activity> mActivity;
 
     /**
      * 打开启动屏
      */
-    public static void show(final Activity activity, final boolean fullScreen, final int themeResId) {
+    public static void show(final Activity activity, final boolean fullScreen) {
         if (activity == null) return;
         mActivity = new WeakReference<Activity>(activity);
         activity.runOnUiThread(new Runnable() {
@@ -29,12 +28,7 @@ public class SplashScreen {
             public void run() {
                 if (!activity.isFinishing()) {
 
-                    mSplashDialog = new Dialog(
-                            activity,
-                            themeResId != NULL_ID ? themeResId
-                                    : fullScreen ? R.style.SplashScreen_Fullscreen
-                                    : R.style.SplashScreen_SplashTheme
-                    );
+                    mSplashDialog = new Dialog(activity, fullScreen ? R.style.SplashScreen_Fullscreen : R.style.SplashScreen_SplashTheme);
                     mSplashDialog.setContentView(R.layout.launch_screen);
                     mSplashDialog.setCancelable(false);
 
@@ -49,15 +43,10 @@ public class SplashScreen {
     /**
      * 打开启动屏
      */
-    public static void show(final Activity activity, final boolean fullScreen) {
-        show(activity, fullScreen, 0);
-    }
-
-    /**
-     * 打开启动屏
-     */
     public static void show(final Activity activity) {
-        show(activity, false);
+        if(mSplashDialog == null){
+            show(activity, false);
+        }
     }
 
     /**
